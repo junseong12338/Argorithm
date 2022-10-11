f90623e88b4a673c9053296340ab1e85c21d4c28
[Github <kimlsy2444@naver.com>
2022-10-11 17:44:12 +0900

    update

--- src/sweaD2/swea_1859.java
+++ src/sweaD2/swea_1859.java
@@ -11,42 +11,29 @@ public class swea_1859 {
 		
 		Scanner sc = new Scanner(System.in);
 		int T = 0;
-		int Number = 0;
-		int day = 0;
-		int largest = 0;
-		int length = 0;
+
 		T=sc.nextInt();
 		
-		int [] Case;
-		for(int test_case = 1; test_case <= T; test_case++)
-		{	
-			day =sc.nextInt();
-			Case = new int[day];
+		
+		for(int TC = 1 ; TC<= T; TC++) {
+			
+			int day = 0;
+			day = sc.nextInt();
+			long money = 0;
+			int Max = Integer.MIN_VALUE;
+			int [] Case = new int[day];
+		
 			for(int i = 0; i<day; i++) {
-				
-				
-				Number = sc.nextInt();
-				Case[i] = Number;
-				System.out.print(Case[i]+" ");
-				
+				Case[i] = sc.nextInt();
 			}
-			  for (int j = 0; j<day;j++) {
-				  
-				if(largest <Case[j] ) {
-					length+=1;
-					largest =Case[j];
+			
+		    for (int j = day-1; j>=0;j--) {
+		    	if(Max < Case[j] ){
+		    		Max = Case[j];
 				}
+				money +=  Max - Case[j];
 			}
-			System.out.println();
-			System.out.println(largest);
-			System.out.println(length);
-			
-			
-		
+		    System.out.println("#"+ TC+" "+ money);
 		}
-		
-	
 	}
-
-
 }
--- src/sweaD3/swea_1217.java
+++ src/sweaD3/swea_1217.java
@@ -1,13 +1,13 @@
 
-package sweaD2;
+package sweaD3;
 
 import java.util.Scanner;
 import java.io.FileInputStream;
-public class swea_2 {
+public class swea_1217 {
 
 	public static void main(String[] args) {
 		
-		// [?] 
+		// [?] [S/W 문제해결 기본] 4일차 - 거듭 제곱
 		
 		Scanner sc = new Scanner(System.in);
 		int T = 0;
