22322defea2677adc62d1e83e485ffe1e4750057
[Github <kimlsy2444@naver.com>
2022-10-25 17:39:15 +0900

    update

---
+++ src/BaekJoon_7/BackJoon_2908.java
@@ -0,0 +1,16 @@
+package BaekJoon_7;
+
+import java.util.Scanner;
+public class BackJoon_2908 {
+	// Main
+public static void main(String[] args) {
+		
+		// [?] 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
