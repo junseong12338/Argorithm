fa5f6de142c0e624a68e5f0ee6586c2f71c2ae19
LeeJunSeong <kimlsy2444@naver.com>
2022-11-01 22:22:20 +0900

    test

---
+++ src/BaekJoon_DoIt/BackJoon_4.java
@@ -0,0 +1,15 @@
+package BaekJoon_DoIt;
+
+
+import java.util.Scanner;
+public class BackJoon_4 {
+	// Main
+	public static void main(String[] args) {
+		
+		// [?] 
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
