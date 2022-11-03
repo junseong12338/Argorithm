793068f88013cf1443a2d4074e66b55d32fa1d55
[Github <kimlsy2444@naver.com>
2022-11-04 00:24:22 +0900

    update

---
+++ src/BaekJoon_DoIt/BaekJoon_Queue_11286_Silver1.java
@@ -0,0 +1,15 @@
+package BaekJoon_DoIt;
+
+
+import java.util.Scanner;
+public class BaekJoon_Queue_11286_Silver1 {
+	// Main
+	public static void main(String[] args) {
+		
+		// [?] 
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
