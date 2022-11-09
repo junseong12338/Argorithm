9c045eec96961db2d5810cb9e7148c05960f44d3
[Github <kimlsy2444@naver.com>
2022-11-09 11:14:03 +0900

    update

--- src/BaekJoon_DoIt/BaekJoon_DFS_BFS_11724_Silver2.java
+++ src/BaekJoon_DoIt/BaekJoon_DFS_BFS_11724_Silver2.java
@@ -6,53 +6,48 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 public class BaekJoon_DFS_BFS_11724_Silver2 {
 	// Main
-
-	static int[][] graph = new int[1001][1001];
-	static int V;
-	static int E;
-	static boolean[] visited = new boolean[1001];
 	
-	public static void dfs(int index) {
-		if(visited[index] == true)
-			return;
-		else {
-			visited[index] = true;
-			for(int i = 1; i <= V; i++) {
-				if(graph[index][i] == 1) {
-					dfs(i);
+	static ArrayList<Integer> A[];
+	static boolean visited[];
+	
+	public static void main(String[] args)throws Exception {
+		Scanner sc = new Scanner(System.in); 
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			visited = new boolean[n+1];
+			A = new ArrayList[n+1];
+			for(int i=1; i<n+1; i++) {
+				A[i] = new ArrayList<Integer>();
+			}
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int s = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				A[s].add(e);
+				A[e].add(s);
+			}
+			int count = 0;
+			
+			for(int i=1; i<n+1; i++) {
+				if(!visited[i]) {
+					count++;
+					DFS(i);
 				}
 			}
-		}
+			System.out.println(count);
 	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
-		
-		V = sc.nextInt();
-		E = sc.nextInt();
-		
-		int a,b;
-		for(int i = 0; i < E; i++) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			
-			// 간선 연결
-			graph[a][b] = graph[b][a] = 1;
+
+	private static void DFS(int v) {
+		if(visited[v]) {
+			return;
 		}
-		
-		int result = 0 ;
-		
-		// dfs 탐색
-		for(int i = 1; i <= V; i++) {
-			if(visited[i] == false) { // 방문한 적 없는 노드라면 dfs.
-				dfs(i);
-				result++;
+		visited[v] = true;
+		for(int i: A[v]) {
+			if(!visited[i]) {
+				DFS(i);
 			}
 		}
-		
-		System.out.println(result);
-		
-		sc.close();
-		return;
 	}
 }
