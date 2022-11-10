a30c15697f39a3871b02da7cea7e55fef365b77f
[Github <kimlsy2444@naver.com>
2022-11-10 10:45:09 +0900

    update

--- src/BaekJoon_DoIt/BaekJoon_Greedy_11047_Silver4.java
+++ src/BaekJoon_DoIt/BaekJoon_Greedy_11047_Silver4.java
@@ -8,29 +8,7 @@ public class BaekJoon_Greedy_11047_Silver4 {
 		
 		// [?] µ¿Àü 0
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] coin = new int[N];
-		int K = sc.nextInt();
-		int count = 0;
 		
-		
-		for(int i=0; i<N;i++) {
-			
-			coin[i] = sc.nextInt();
-		}
-		int C = coin.length-1;
-		while(0 < K) {
-			if(K >= coin[C]) {
-				count += K/coin[C];
-				K %= coin[C];
-				C--;
-			}
-			else {
-				C--;
-			}
-			
-		}
-		System.out.println(count);
 	}
 
 }
--- src/BaekJoon_DoIt/BaekJoon_Search_Binary_1920_Silver4.java
+++ src/BaekJoon_DoIt/BaekJoon_Search_Binary_1920_Silver4.java
@@ -10,32 +10,43 @@ public class BaekJoon_Search_Binary_1920_Silver4 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		
-		int[] arr = new int[N];
-		for(int i=0;i<N;i++) arr[i] = sc.nextInt();	
 		
-		Arrays.sort(arr);
-		
+		int A [] = new int[N];
+		for(int i=0; i<N; i++) {
+			A[i] = sc.nextInt();
+		}
+		Arrays.sort(A);
 		int M = sc.nextInt();
-		
-		int[] arr2 = new int[M];
-		for(int i=0;i<M;i++) arr2[i] = sc.nextInt();
-		
-		for(int i=0;i<M;i++) {
-			System.out.println(binarySearch(arr, arr2[i]));
-		}
-	}
-	public static int binarySearch(int[] arr, int num) {
-		int low = 0;
-		int high = arr.length-1;
-		int mid = 0;
-		
-		while(low<=high) {
-			mid = (low + high) / 2;
-			if(arr[mid]==num) return 1;
+		for(int i=0; i<M;i++) {
+			int target = sc.nextInt();
+			int start = 0;
+			int end = N-1;
+			boolean flag = false;
+			while(start <= end) {
+				int mid = (start + end) /2;
+				int midv = A[mid];
+				if(midv > target) {
+					end = mid - 1;
+					
+					
+				}
+				else if(midv < target) {
+					start = mid + 1;
+					
+				}
+				else {
+					flag = true;
+					break;
+				}
+			}
+			if(flag == true) {
+				System.out.println(1);
+			}
+			else {
+				System.out.println(0);
+			}
 			
-			else if(arr[mid]>num) high = mid-1;
-			else if(arr[mid]<num) low = mid + 1;
 		}
-		return 0;
+	
 	}
 }
