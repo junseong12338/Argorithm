a44b7f74bdf8e532a9417f25a81b1d8866af3b8a
[Github <kimlsy2444@naver.com>
2023-01-19 20:30:10 +0900

    1.19

---
+++ src/Practice/BaekJoon14.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import java.util.Scanner;
+// Main
+public class BaekJoon14 {
+	public static void main(String[] args) {
+		
+		// [?] 
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
