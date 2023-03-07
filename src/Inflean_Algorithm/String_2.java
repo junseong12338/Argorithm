7713bd44d48f3ff5a62ac588367f1b4da7d098fe
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-03-08 04:37:08 +0900

    03.08

---
+++ src/Inflean_Algorithm/String_2.java
@@ -0,0 +1,30 @@
+package Inflean_Algorithm;
+
+import java.util.Scanner;
+
+/*2. 대소문자 변환
+설명
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+입력
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+*/
+
+//Main
+public class String_2 {
+	
+	public static String solution(String str) {
+		
+	
+		return "";
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		solution(str);
+		
+		return;
+	}
+
+}
