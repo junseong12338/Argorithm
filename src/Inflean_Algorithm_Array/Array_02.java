ab9d0a4bf33849939b81baf0aae223610d2760b5
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-05-11 21:23:34 +0900

    0511

--- src/Inflean_Algorithm_Array/Array_02.java
+++ src/Inflean_Algorithm_Array/Array_02.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 //Main
-public class Array_01 {
+public class Array_02 {
 
 	public static void main(String[] args) {
 
@@ -15,22 +15,22 @@ public class Array_01 {
 		int count = sc.nextInt();
 		
 		int N[] = new int [count];
-		
+		int check = 1;
 		for(int i = 0; i < count;i++) {N[i] =sc.nextInt();}
 		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		arr.add(N[0]);
-		
-		for(int i = 1; i < count; i++) {
-
-			if(N[i] > N[i-1]) {
-				arr.add(N[i]);
-			}
+		int tmp = N[0];
+		for(int i = 1; i < count;i++) {
 			
+			if(tmp < N[i]) {
+				tmp = N[i];
+				check++;
+			} 
 		}
-		for(Integer num : arr)System.out.print(num+" ");
+		System.out.println(check);
+		
 	
-		return;
+	
+	
 	}
 
 }
---
+++ src/Inflean_Algorithm_Array/Array_03.java
@@ -0,0 +1,32 @@
+package Inflean_Algorithm_Array;
+
+import java.util.Scanner;
+
+
+
+//Main
+public class Array_03 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		int A[] = new int [count];
+		int B[] = new int [count];
+		
+		for(int i = 0; i < count;i++) {A[i] =sc.nextInt();}
+		
+		for(int i = 0; i < count;i++) {B[i] =sc.nextInt();}
+		
+		
+		for(int i = 0; i < count;i++) {
+			
+			
+			
+			
+		}
+	}
+
+}
