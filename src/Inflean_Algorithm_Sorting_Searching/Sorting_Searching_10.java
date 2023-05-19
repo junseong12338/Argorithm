cc12210dc06d03bc8b38d51774fb4baa66e7d892
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-05-19 16:30:41 +0900

    0519

---
+++ src/Inflean_Algorithm_Sorting_Searching/Sorting_Searching_10.java
@@ -0,0 +1,57 @@
+package Inflean_Algorithm_Sorting_Searching;
+
+
+import java.util.*;
+
+//Main
+public class Sorting_Searching_10 {
+	
+	public static int count (int [] arr, int dist) {
+		
+		int cnt = 1;
+		int ep = arr[0];
+		for(int  i = 1; i <arr.length; i++) {
+			if(arr[i]-ep >= dist) {
+				cnt++;
+				ep = arr[i];
+			}
+		}
+		return cnt;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();		
+		int arr [] = new int [n];
+		
+		
+		for(int i = 0 ; i < n; i++) {
+			arr[i] = sc.nextInt();
+			
+		}
+		
+		int answer = 0;
+		Arrays.sort(arr);
+		
+		int lt =1;
+		int rt = arr[n-1];
+		while(lt<=rt){
+			int mid = (lt+rt)/2;
+			if (count (arr,mid)>=m) {
+				answer = mid;
+				lt = mid+1;
+			}else {
+				rt = mid-1;
+			}
+		}
+	
+		
+
+		System.out.println(answer);
+
+	}
+}
