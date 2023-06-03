ce404d147d46174a2d3efc3f6060299caa12a093
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-06-03 23:14:45 +0900

    06.03

---
+++ src/BaekJoon_Algorithm_Data_Structure/BaekJoon_2.java
@@ -0,0 +1,13 @@
+package BaekJoon_Algorithm_Data_Structure;
+
+import java.util.Scanner;
+public class BaekJoon_2 {
+	// Main
+	public static void main(String[] args) {
+		
+		// [?]
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
