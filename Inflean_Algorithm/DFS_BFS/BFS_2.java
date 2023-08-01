89684634b79bd76a0d40f837aabc8ca8189b0230
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-08-01 11:30:03 +0900

    08.01

---
+++ Inflean_Algorithm/DFS_BFS/BFS_2.java
@@ -0,0 +1,49 @@
+package DFS_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// 송아지 찾기 BFS
+
+public class BFS_2 {
+	static int answer = 0;
+	static int dis[] = {1,-1,5};
+	static int ch [] = new int [100001];
+	static Queue<Integer> Q = new LinkedList<Integer>();
+	public static int BFS(int s, int e) {
+	
+		ch[s] = 1;
+		Q.offer(s);
+		int L = 0;
+		
+		while(!Q.isEmpty()) {
+			int len = Q.size();
+			for(int i = 0; i<len; i++) {
+				int x = Q.poll();
+				for(int j = 0; j <3; j++) {
+					int nx = x+dis[j];
+					if(nx == e) return L+1;
+					if(nx > 0 && nx <= 100000 && ch[nx] == 0) {
+	
+						ch[nx] = 1;
+						Q.offer(nx);
+					}
+				}
+			}
+			L++;
+		}
+		return L;
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int s = sc.nextInt();
+		int e = sc.nextInt();
+		
+		System.out.println(BFS(s,e));
+	}
+}
+
+
