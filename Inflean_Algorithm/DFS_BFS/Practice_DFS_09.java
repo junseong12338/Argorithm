9c6b1f3a879ded0e2b904e77eb493687524b57b9
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-03 14:41:17 +0900

    09.03

--- Inflean_Algorithm/DFS_BFS/Practice_DFS_09.java
+++ Inflean_Algorithm/DFS_BFS/Practice_DFS_09.java
@@ -6,15 +6,29 @@ import java.util.*;
 // Main 
 public class Practice_DFS_09 {
 	
+	public static int n,m;
+	public static int[] combi;
 	
-	public static void DFS(int L, int sum) {
-	
+	public static void DFS(int L, int s) {
+		
+		if(L==m) {
+			for(int x : combi) System.out.print(x+" ");
+			System.out.println();
+		}else {
+			for(int i=s; i <=n; i++) {
+				combi[L]=i;
+				DFS(L+1,i+1);
+			}
+		}
 	}
 
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		 n = sc.nextInt();
+		 m = sc.nextInt();
+		 combi = new int[m];
+		 DFS(0,1);
 	
 	}
 }
--- Inflean_Algorithm/DFS_BFS/Practice_DFS_10.java
+++ Inflean_Algorithm/DFS_BFS/Practice_DFS_10.java
@@ -2,33 +2,23 @@ package DFS_BFS;
 
 import java.util.*;
 
-// 미로탐색(DFS)
+// 미로탐색(DFS) 경로의 가지수
 // Main 
 public class Practice_DFS_10 {
-	static int[] b,p,ch;
-	static int n,f;
-	static boolean falg = false;
-	static int [][] dy = new int[35][35];
-	public static int combi(int n, int r) {
-		if (dy[n][r] >0)return dy[n][r];
-		if(n==r|| r ==0) return 1;
-		else return dy[n][r] = combi(n-1,r-1) + combi(n-1, r);
-	}
-	
-	public static void DFS(int L, int sum) {
-		if(falg) return;
-		if(L==n) {
-			if(sum == f) {
-				for(int x :p) System.out.print(x+" ");
-				falg = true;
-			}
-		}else {
-			for(int i = 1; i<=n;i++) {
-				if(ch[i] == 0) {
-					ch[i]=1;
-					p[L] = i;
-					DFS(L+1,sum+(p[L]*b[L]));
-					ch[i]=0;
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	static int[][] board;
+	static int answer = 0;
+	public static void DFS(int x, int y) {
+		if(x==7 && y==7) answer++;
+		else {
+			for(int i =0; i<4; i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(nx>=1 && nx<=7&& ny>=1 && ny<=7 && board[nx][ny]==0) {
+					board[nx][ny]=1;
+					DFS(nx,ny);
+					board[nx][ny]=0;
 				}
 			}
 		}
@@ -37,14 +27,14 @@ public class Practice_DFS_10 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		f = sc.nextInt();
-		b = new int[n];
-		p = new int[n];
-		ch = new int[n+1];
-		for(int i =0; i <n; i++) {
-			b[i] = combi(n-1,i);
+		board = new int[8][8];
+		for(int i =1; i<=7; i++) {
+			for(int j=1; j<=7; j++) {
+				board[i][j] = sc.nextInt();
+			}
 		}
-		DFS(0,0);
+		board[1][1] = 1;
+		DFS(1,1);
+		System.out.println(answer);
 	}
 }
