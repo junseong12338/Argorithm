2c98cec4c6eb03a49afa05aabcb04136ee2f907d
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-05 17:38:26 +0900

    09.05

---
+++ Inflean_Algorithm/DFS_BFS/Practice_BFS_14.java
@@ -0,0 +1,63 @@
+package DFS_BFS;
+
+import java.util.*;
+
+// 섬나라 아일랜드 (BFS)
+// Main 
+class Point{
+	public int x,y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+public class Practice_BFS_14 {
+	public static int  answer = 0,n;
+	public static int[] dx = {-1,-1,0,1,1,1,0,-1};
+	public static int[] dy = {0,1,1,1,0,-1,-1,-1};
+	
+	public static Queue<Point> queue = new LinkedList<Point>();
+	
+	public static void BFS(int x, int y,int[][] board) {
+		queue.add(new Point(x,y));
+		while(!queue.isEmpty()) {
+			Point pos = queue.poll();
+			
+			for(int i=0; i<8;i++) {
+				int nx=pos.x+dx[i];
+				int ny=pos.y+dy[i];
+				if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1) {
+					board[nx][ny] = 0;
+					queue.add(new Point(nx,ny));
+				}
+			}
+		}
+
+	}
+	public static void solution(int[][] board) {
+		for(int i =0; i<n; i++ ) {
+			for(int j=0; j<n;j++) {
+				if(board[i][j] ==1) {
+					answer++;
+					board[i][j] = 0;
+					BFS(i,j,board);
+				}
+			}
+		}
+		
+	}
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		for(int i =0; i <n; i++) {
+			for(int j=0; j<n;j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		solution(arr);
+		System.out.println(answer);
+	}
+}
---
+++ Inflean_Algorithm/DFS_BFS/Practice_DFS_13.java
@@ -0,0 +1,48 @@
+package DFS_BFS;
+
+import java.util.*;
+
+// 섬나라 아일랜드 (DFS)
+// Main 
+public class Practice_DFS_13 {
+	public static int  answer = 0,n;
+	public static int[] dx = {-1,-1,0,1,1,1,0,-1};
+	public static int[] dy = {0,1,1,1,0,-1,-1,-1};
+
+	public static void DFS(int x, int y,int[][] board) {
+		for(int i=0; i<8;i++) {
+			int nx=x+dx[i];
+			int ny=y+dy[i];
+			if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1) {
+				board[nx][ny] =0;
+				DFS(nx,ny,board);
+			}
+		}
+	}
+	public static void solution(int[][] board) {
+		for(int i =0; i<n; i++ ) {
+			for(int j=0; j<n;j++) {
+				if(board[i][j] ==1) {
+					answer++;
+					board[i][j] = 0;
+					DFS(i,j,board);
+				}
+			}
+		}
+		
+	}
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int[][] arr = new int[n][n];
+		for(int i =0; i <n; i++) {
+			for(int j=0; j<n;j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		solution(arr);
+		System.out.println(answer);
+	}
+}
