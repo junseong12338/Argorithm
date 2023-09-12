89560423c3711186d26eeeeada95af65303f857d
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-12 22:18:34 +0900

    0912

---
+++ BaekJoon_Algorithm/BaekJoon_Algorithm_DFS_BFS/BaekJoon_DFS_2.java
@@ -0,0 +1,14 @@
+package BaekJoon_Algorithm_DFS_BFS;
+
+import java.util.*;
+import java.io.*;
+public class BaekJoon_DFS_2 {
+	// Main
+	public static void main(String[] args) {
+		
+		// [?] 
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
