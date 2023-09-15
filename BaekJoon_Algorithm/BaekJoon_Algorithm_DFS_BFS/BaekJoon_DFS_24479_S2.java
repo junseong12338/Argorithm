915cfa80a865a42c2646fd8ffdcace8bf5d97c3d
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-15 23:51:04 +0900

    09.15

---
+++ BaekJoon_Algorithm/BaekJoon_Algorithm_DFS_BFS/BaekJoon_DFS_24479_S2.java
@@ -0,0 +1,14 @@
+package BaekJoon_Algorithm_DFS_BFS;
+
+import java.util.*;
+import java.io.*;
+public class BaekJoon_DFS_24479_S2 {
+	// Main
+	public static void main(String[] args) {
+		
+		// [?] 알고리즘 수업 깊이우선 탐색 1 S2
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
