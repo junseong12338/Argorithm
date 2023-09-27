9e00e47dd05e62bf368f303e7bab93af18f22f67
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-27 18:29:39 +0900

    09.27

--- Inflean_Algorithm/Greedy/Greedy_07.java
+++ Inflean_Algorithm/Greedy/Greedy_07.java
@@ -2,12 +2,67 @@ package Greedy;
 
 import java.util.*;
 import java.io.*;
+
+class Edges implements Comparable<Edges>{
+	public int v1;
+	public int v2;
+	public int cost;
+
+	
+	public Edges(int v1, int v2, int cost) {
+		this.v1 = v1;
+		this.v2 = v2;
+		this.cost = cost;
+		
+	}
+	@Override
+	public int compareTo(Edges o) {
+		return this.cost-o.cost;
+	}
+}
 //Main
 public class Greedy_07 {
+	static int[] unf;
+
 	public static void main(String[] args) {
-		
+		// 최소스패닝트리 : 크루스칼, Uinon&Find 활용
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt(); 
+		int answer = 0;
+		unf = new int[n+1];
+		ArrayList<Edges> arr = new ArrayList<Edges>();
+		for(int i = 1; i <=n; i++) unf[i] = i;
+		for(int i = 0; i <m; i++){
 			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			arr.add(new Edges(a,b,c));
+			
+		}
+		Collections.sort(arr);
+		for(Edges ob : arr) {
+			int fv1 = Find(ob.v1);
+			int fv2 = Find(ob.v2);
+			if (fv1 != fv2) {
+				answer+=ob.cost;
+				Union(ob.v1,ob.v2);
+			}
+		}
+		
+		
+		System.out.println(answer);
+	}
+	
+	public static int Find(int v) {
+		if(v == unf[v]) return v;
+		else return unf[v] = Find(unf[v]);
+	}
+	public static void Union(int a, int b) {
+		int fa = Find(a);
+		int fb = Find(b);
+		if(fa != fb) unf[fa] = fb;
 	}
 
 }
---
+++ Inflean_Algorithm/Greedy/Greedy_08.java
@@ -0,0 +1,13 @@
+package Greedy;
+
+import java.util.*;
+import java.io.*;
+//Main
+public class Greedy_08 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+			
+	}
+
+}
