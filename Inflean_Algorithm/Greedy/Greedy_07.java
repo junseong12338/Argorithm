8492fd6ca449d5a60749cb2da7f70ecd8e488338
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-09-27 17:39:55 +0900

    09.27

---
+++ Inflean_Algorithm/Greedy/Greedy_07.java
@@ -0,0 +1,13 @@
+package Greedy;
+
+import java.util.*;
+import java.io.*;
+//Main
+public class Greedy_07 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+			
+	}
+
+}
