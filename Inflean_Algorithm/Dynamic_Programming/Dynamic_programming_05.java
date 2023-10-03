308bbee599fc82d44330a02fe4e335c0da5da2fe
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-03 21:24:31 +0900

    10.03

--- Inflean_Algorithm/Dynamic_Programming/Dynamic_programming_05.java
+++ Inflean_Algorithm/Dynamic_Programming/Dynamic_programming_05.java
@@ -5,10 +5,29 @@ import java.util.*;
 //Main
 public class Dynamic_programming_05 {
 	// 동전 교환 냅색 알고리즘
+	static int n,m;
+	static int[] dy;
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-				
+		n = sc.nextInt();
+		int [] arr = new int[n];
+		for(int i = 0; i < n ; i++) arr[i] = sc.nextInt();
+		m = sc.nextInt();
+		dy = new int[m+1];
+		System.out.println(solution(arr));
 	}
+	
+	public static int solution(int[] coin) {
+		Arrays.fill(dy,Integer.MAX_VALUE);
+		dy[0] = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = coin[i]; j<=m; j++) {
+				dy[j] = Math.min(dy[j],dy[j-coin[i]]+1);
+			}
+		}
+		return dy[m];
+	}
+
 
 }
--- Inflean_Algorithm/Dynamic_Programming/Dynamic_programming_06.java
+++ Inflean_Algorithm/Dynamic_Programming/Dynamic_programming_06.java
@@ -7,8 +7,21 @@ public class Dynamic_programming_06 {
 	// 최대 점수 구하기 냅색 알고리즘
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-				
+	Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int [] dy = new int [m+1];
+		for(int i = 0; i <n; i++) {
+			int ps = sc.nextInt();
+			int pt = sc.nextInt();
+			for(int j = m; j>=pt; j--) {
+				dy[j] = Math.max(dy[j],dy[j-pt] + ps);
+			}
+			
+		}
+		System.out.println(dy[m]);
+		
 	}
 
 }
