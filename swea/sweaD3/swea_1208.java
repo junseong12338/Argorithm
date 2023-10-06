2b7df2ca0d528eecfed9feb68c28460cc63b462b
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-06 19:31:55 +0900

    10.06

--- swea/sweaD3/swea_1208.java
+++ swea/sweaD3/swea_1208.java
@@ -7,7 +7,7 @@ public class swea_1208 {
 	public static void main(String[] args) {
 		
 		// [?] [S/W 문제해결 기본] 1일차 - Flatten
-		
+		 
 		Scanner sc = new Scanner(System.in);
 		int T = 0;
 		T=sc.nextInt();
