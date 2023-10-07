4c153ba3c22a6d1b8c05942878249a6bdb633460
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-07 10:49:55 +0900

    10.07

---
+++ swea/sweaD3/swea_1491.java
@@ -0,0 +1,24 @@
+package sweaD3;
+
+import java.util.Scanner;
+
+public class swea_1491 {
+
+	public static void main(String[] args) {
+		
+		// [?] 1491. 원재의 벽 꾸미기
+
+		
+		Scanner sc = new Scanner(System.in);
+		int T = 0;
+		T=sc.nextInt();
+		int N = 0;
+		
+		for(int TC = 1 ; TC<= T; TC++) {
+			
+		}
+		
+	
+	}
+
+}
