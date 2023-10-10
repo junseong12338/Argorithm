6ec8e4dd4034bb7b37213c28fae7b64182339766
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-10 15:50:41 +0900

    10.10

---
+++ swea/sweaD3/swea_1215.java
@@ -0,0 +1,83 @@
+package sweaD3;
+
+import java.util.Scanner;
+
+public class swea_1215 {
+
+	public static void main(String[] args) {
+		
+		// [?] 1215. [S/W 문제해결 기본] 3일차 - 회문1
+		
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+			
+		for(int TC = 1 ; TC<= T; TC++) {
+			int count = 0;
+			int n = sc.nextInt();
+			String map1[] = new String[8];	
+			String map2[] = new String[8];	
+			char ch[][] = new char[8][8];
+			int ck = n-1;
+			
+			for(int i = 0; i < 8; i++) {
+				map1[i] = sc.next();
+				map2[i] ="";
+				for(int j = 0; j < 8; j++) {
+					ch[i][j] = map1[i].charAt(j);
+				}
+			}
+			// 세로 값 입력
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					map2[i] += ch[j][i];
+				}
+			}
+		
+				
+			
+			// 가로 계산
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					if(n+j <= 8) {
+						String str = (String) map1[i].subSequence(j, n+j);
+						for(int k = 0; k < str.length(); k++) {
+							
+							if (str.charAt(k) == str.charAt(ck) && ck > 0) ck--;
+							else {
+								ck  = n-1;
+								break;
+							}
+							if(ck == 0) count++;
+						}
+					}
+				}
+			
+			}
+			
+			ck = n-1;
+			// 세로 계산
+			
+			for(int i = 0; i < 8; i++) {
+				for(int j = 0; j < 8; j++) {
+					if(n+j <= 8) {
+						
+						String str = (String) map2[i].subSequence(j, n+j);
+						for(int k = 0; k < str.length(); k++) {
+							if (str.charAt(k) == str.charAt(ck) && ck > 0) ck--;
+							else {
+								ck  = n-1;
+								break;
+							}
+							
+							if(ck == 0) count++;
+						}
+					}
+				}
+			}			
+			
+			System.out.println("#"+TC+" "+count);
+			
+			
+		}
+	}
+}
--- swea/sweaD3/swea_1216.java
+++ swea/sweaD3/swea_1216.java
@@ -2,12 +2,12 @@ package sweaD3;
 
 import java.util.Scanner;
 
-public class swea_1491 {
+public class swea_1216 {
 
 	public static void main(String[] args) {
 		
-		// [?] 1491. 원재의 벽 꾸미기
-
+		// [?] 
+		
 		Scanner sc = new Scanner(System.in);
 		int T = 0;
 		T=sc.nextInt();
