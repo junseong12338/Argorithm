22f8845a093ec2bffb6a5f9b4191b5a1450cf4f5
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-11 14:06:28 +0900

    10.11

--- swea/sweaD3/swea_1217.java
+++ swea/sweaD3/swea_1217.java
@@ -3,22 +3,34 @@ package sweaD3;
 import java.util.Scanner;
 
 public class swea_1217 {
-
+	static int answer = 0;
 	public static void main(String[] args) {
 		
 		// [?] 1217. [S/W 문제해결 기본] 4일차 - 거듭 제곱
-
-		
 		Scanner sc = new Scanner(System.in);
-		int T = 0;
-		T=sc.nextInt();
-		int N = 0;
-		
+		int T = 10;
 		for(int TC = 1 ; TC<= T; TC++) {
+			int N = sc.nextInt();
+			
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			System.out.println("#"+TC+" "+power(n,m));
 			
 		}
 		
 	
+	}
+
+	private static int power(int n, int m) {
+		 m--;
+		if (m < 1) {
+			return answer;
+		}
+		 answer = n;
+		
+		
+		return answer*=power(n,m);
 	}
 
 }
---
+++ swea/sweaD3/swea_1220.java
@@ -0,0 +1,23 @@
+package sweaD3;
+
+import java.util.Scanner;
+
+public class swea_1220 {
+
+	public static void main(String[] args) {
+		
+		// [?] 1220. [S/W 문제해결 기본] 5일차 - Magnetic
+		
+		Scanner sc = new Scanner(System.in);
+		int T = 0;
+		T=sc.nextInt();
+		int N = 0;
+		
+		for(int TC = 1 ; TC<= T; TC++) {
+			
+		}
+		
+	
+	}
+
+}
