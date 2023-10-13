fdc4696e69f8d08c25c8403da057e6a5011df79b
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-13 20:22:55 +0900

    10.13

--- swea/sweaD3/swea_1228.java
+++ swea/sweaD3/swea_1228.java
@@ -14,55 +14,12 @@ public class swea_1228 {
 		for(int TC = 1 ; TC<= T; TC++) {
 			
 			int N = sc.nextInt();
-			String pass[] = new String [N];
-			
-			for(int i = 0; i < N; i++) {
-				pass[i] = sc.next();
+
+
+			System.out.print("#"+TC);
+			for(int i = 0; i < 10; i++) {
 			}
-
-			int oder = sc.nextInt();
-			
-		
-			for(int i = 0; i < oder; i++) {
-				String sys = sc.next();
-				int s = sc.nextInt();
-				int e = sc.nextInt();
-				String p[] = new String [e];
-				
-				for(int j = 0; j < e; j++) {
-					p[j] = sc.next();
-				}
-				if(s < 10) {
-					int num = 0;
-					
-					for(int k = s; k < 10-e; k++) {
-						System.out.println(k+s);
-						String temp = pass[num];
-						pass[k+e] = temp;
-						num++;
-					}
-//					for(int j = 0; j < e; j++) {
-//						pass[j] = p[j];
-//					}
-					for(int k = 0; k < 10; k++) {
-						System.out.print(pass[k]+" ");
-					}
-					System.out.println();
-				}
-
-			}
-		
-			// 566753 244528 233616 328235 268163 292641 646305 944392 643695 385788
-			
-			// 566753 244528 233616 328235 566753 244528 233616 328235 268163 292641
-			
-			// 600576 565945 486128 594841 566753 244528 233616 328235 268163 292641
-
-//			System.out.print("#"+TC);
-//			for(int i = 0; i < 10; i++) {
-//				System.out.print(" "+pass[i]);
-//			}
-//			System.out.println();
+			System.out.println();
 		}
 	}
 }
