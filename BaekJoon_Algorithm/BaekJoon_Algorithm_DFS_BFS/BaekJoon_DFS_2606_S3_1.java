0a0fd49845b86373de35e7c04c9eaf8845756261
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-20 13:13:09 +0900

    10.20

--- BaekJoon_Algorithm/BaekJoon_Algorithm_DFS_BFS/BaekJoon_DFS_2606_S3_1.java
+++ BaekJoon_Algorithm/BaekJoon_Algorithm_DFS_BFS/BaekJoon_DFS_2606_S3_1.java
@@ -2,43 +2,47 @@ package BaekJoon_Algorithm_DFS_BFS;
 
 import java.util.*;
 import java.io.*;
-public class BaekJoon_DFS_2606_S3 {
+public class BaekJoon_DFS_2606_S3_1 {
 	// Main
+
 	static boolean visited[];
-	static ArrayList<Integer>[] A ;
+	static ArrayList<Integer> A[];
 	static int count = 0;
 	public static void main(String[] args)  throws IOException{
 		
 		// [?] 2606 바이러스 S3
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int V = sc.nextInt(); // 시작점
 		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		visited = new boolean[n+1];
-		A = new ArrayList [n+1];
+		A = new ArrayList [N+1];
+		visited = new boolean [N+1];
 		
-		for(int i = 1; i <= n; i++) A[i] = new ArrayList<Integer>();
-		
-		for(int i = 0; i < m; i++) {
-			int s = sc.nextInt();
-			int e = sc.nextInt();
-			A[s].add(e);
-			A[e].add(s);
+		for(int i = 1; i <= N; i++) A[i] = new ArrayList<Integer>();
+
+		for(int i = 0 ; i < V; i++) {
+			int S = sc.nextInt();
+			int E = sc.nextInt();
+			A[S].add(E);
+			A[E].add(S);
 		}
-	
 		DFS(1);
 		System.out.println(count);
+	
 	}
 	private static void DFS(int v) {
 		if(visited[v]) return;
 		visited[v] = true;
+		
 		for(int i : A[v]) {
 			if(!visited[i]) {
-				count++;
+				count ++;
 				DFS(i);
 			}
 		}
+	
 		
 	}
-
+	
+	
 }
---
+++ BaekJoon_Algorithm/BaekJoon_Algorithm_DFS_BFS/BaekJoon_DFS_2667_S1_1.java
@@ -0,0 +1,17 @@
+package BaekJoon_Algorithm_DFS_BFS;
+
+import java.util.*;
+import java.io.*;
+public class BaekJoon_DFS_2667_S1_1 {
+	// Main
+
+	
+	public static void main(String[] args) {
+		 
+		
+		// [?] 단지번호붙이기 S1 -- 2
+	
+		
+	}
+
+}
