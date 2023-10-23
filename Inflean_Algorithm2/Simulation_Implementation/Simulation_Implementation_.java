39d2922b9fb386ce7fbb577b40d1f2804f763949
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-23 17:06:06 +0900

    10.23

---
+++ Inflean_Algorithm2/Simulation_Implementation/Simulation_Implementation_.java
@@ -0,0 +1,14 @@
+package Simulation_Implementation;
+
+import java.util.*;
+
+//Main
+public class Simulation_Implementation_ {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+				
+	}
+
+}
---
+++ Inflean_Algorithm2/Simulation_Implementation/Simulation_Implementation_01.java
@@ -0,0 +1,30 @@
+package Simulation_Implementation;
+
+import java.util.*;
+
+//Main 사다리 타기
+public class Simulation_Implementation_01 {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(Arrays.toString(solution(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+		System.out.println(Arrays.toString(solution(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+		System.out.println(Arrays.toString(solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+		System.out.println(Arrays.toString(solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+	}
+
+		
+		public static char[] solution(int n, int[][] ladder){
+			char[] answer = new char[n];
+
+			
+			
+			
+			
+			
+			return answer;
+		}
+
+
+	}
