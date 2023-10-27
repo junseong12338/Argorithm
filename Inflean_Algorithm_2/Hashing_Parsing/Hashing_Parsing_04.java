d568b81a1622dd28547a1f59d04fefd88bdb690b
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-10-27 21:07:35 +0900

    10.27

--- Inflean_Algorithm_2/Hashing_Parsing/Hashing_Parsing_04.java
+++ Inflean_Algorithm_2/Hashing_Parsing/Hashing_Parsing_04.java
@@ -5,10 +5,17 @@ import java.util.*;
 //Main
 public class Hashing_Parsing_04 {
 	
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-				
+	public static int solution(int[] nums, int m){
+		int answer = 0;
+		
+		return answer;
 	}
 
+	public static void main(String[] args){
+		System.out.println(solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));	
+		System.out.println(solution(new int[]{1, 2, 3, -3, 1, 2, 2, -3}, 5));	
+		System.out.println(solution(new int[]{1, 2, 3, -3, 1, 2}, 3));	
+		System.out.println(solution(new int[]{-1, 0, 1}, 0));	
+		System.out.println(solution(new int[]{-1, -1, -1, 1}, 0));	
+	}
 }
