7a8bfb0b8a4dd942c07755a8305ed4b9a3c9044f
LeeJunSeong <kimlsy2444@naver.com>
2023-10-29 13:52:53 +0900

    10.29

--- Inflean_Algorithm_2/Hashing_Parsing/Hashing_Parsing_04.java
+++ Inflean_Algorithm_2/Hashing_Parsing/Hashing_Parsing_04.java
@@ -5,11 +5,25 @@ import java.util.*;
 //Main
 public class Hashing_Parsing_04 {
 	
-	public static int solution(int[] nums, int m){
+	public static int solution(int[] nums , int m){
+		HashMap<Integer, Integer> sH = new HashMap<Integer, Integer>();
 		int answer = 0;
+		int sum= 0;
+		
+		
+		sH.put(0,1);
+	
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if(sH.containsKey(sum-m)) answer += sH.get(sum-m);
+			sH.put(sum,sH.getOrDefault(sum,0) +1);
+		
+		}
+
 		
 		return answer;
 	}
+
 
 	public static void main(String[] args){
 		System.out.println(solution(new int[]{2, 2, 3, -1, -1, -1, 3, 1, 1}, 5));	
