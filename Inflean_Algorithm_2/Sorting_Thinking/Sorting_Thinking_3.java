b77612b930a92c2154d13671703b3f0d268ad654
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-11-05 06:34:23 +0900

    11.05

--- Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_3.java
+++ Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_3.java
@@ -7,9 +7,47 @@ public class Sorting_Thinking_3 {
 
 	public static int solution(int[] nums, int k){
 		int answer = 0;
+		int n = nums.length;
+		int diff [][] = new int [nums.length/2][2];
+		Integer[] arr = new Integer[nums.length];
 		
-		return answer;
+		for(int i = 0; i < nums.length; i++) arr[i] = nums[i];
+		
+		Arrays.sort(arr,Collections.reverseOrder());
+		
+		// 2차원 배열을 이용하여 diff[해당 라운드] [차이 값]
+		// 즉 0 ~ 4 라운드라고 가정 했을시
+		// diff [0 : 0] [1 : 1] [2 : 2] [3 : 2] [4 : 0] 
+		for (int i = 0; i < n - 1; i += 2) {
+	        diff[i / 2][0] = i / 2;
+	        diff[i / 2][1] = arr[i] - arr[i + 1];
+	    }
+		
+		// 이후 내림 차순으로 정렬 하여
+		// diff [2 : 2] [3 : 2] [1 : 1] [0 : 0] [4 : 0] 
+		Arrays.sort(diff, Comparator.comparingInt((int[] o) -> o[1]).reversed());
+
+		for (int i = 0; i < k; i++) {
+	        // selectedCardIndex 선택된 카드 줄여서 SCI
+			// 우선권(k)번 만큼 반복을 하며
+			// 우선권이 적용된 카드를 선택하여 answer에 더한다
+	        int SCI = diff[i][0] * 2;
+	        answer += arr[SCI];
+			// 우선권 을 사용 하지 않았을시 선택 돼야 할 값을 0으로 변경한다. 
+
+	        arr[SCI+1] = 0;
+	    }
+
+		// 우선권이 이루어지지 않았을 때의 선택된 값들을 더한다. 우선권을 적용했다면 0을 더해주기 때문에 값의 변경은 없다.
+	    for (int i = 0; i < n / 2; i++) {
+	    	int SCI = diff[i][0] * 2+1;
+	        answer += arr[SCI];
+	    }
+
+	    return answer;
 	}
+		
+
 
 	public static void main(String[] args){
 		
