976be0850a24a5cafc836e3d8164304245b4c24b
LeeJunSeong <kimlsy2444@naver.com>
2023-11-05 23:22:05 +0900

    11.05

--- Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_5.java
+++ Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_5.java
@@ -6,6 +6,26 @@ import java.util.*;
 public class Sorting_Thinking_5 {
 	public static int solution(int[][] board){
 		int answer=0;
+		int n = board.length;
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		ArrayList<Integer> col = new ArrayList<Integer>();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(board[i][j] == 1) {
+					row.add(i);
+					col.add(j);
+				}
+			}
+		}
+		// 오름 차순 정렬
+		col.sort((a,b) -> a - b);
+		int x = row.get(row.size()/2);
+		int y = col.get(row.size()/2);
+
+		for(int p : row ) answer += Math.abs(x - p);
+		for(int p : col ) answer += Math.abs(y - p);
+
 		
 		return answer;
 	}
--- Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_6.java
+++ Inflean_Algorithm_2/Sorting_Thinking/Sorting_Thinking_6.java
@@ -6,9 +6,30 @@ import java.util.*;
 public class Sorting_Thinking_6 {
 	  public static int solution(int[] tasks, long k) {
       	int answer = 0;
-		
-		return answer;
-  }
+      	int[] sT = new int[tasks.length + 1];
+      	System.arraycopy(tasks, 0, sT, 1, tasks.length);
+      	Arrays.sort(sT);
+      	int rest = tasks.length;
+      	for(int i = 1; i < sT.length; i++){
+      		long time = ((long)rest * (sT[i] - sT[i-1]));
+      		if(k < time){
+	      		long idx = k % rest;
+	      		int cnt = 0;
+	      		for(int j = 0; j < tasks.length; j++){
+		      		if(tasks[j] >= sT[i]){
+		      			if(cnt == idx) return j+1;
+			      			cnt++;
+			      		}
+		      		}
+	      		}
+	      		else{
+	      		k -= time;
+	      		rest--;
+      		}
+      	}
+      	return -1;
+	  }	
+      		
 
 	public static void main(String[] args){
 	
