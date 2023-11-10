74257ce961695a7a43294fe98ecd6c457840bb6b
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-11-10 19:23:10 +0900

    11.10

--- Inflean_Algorithm_2/DFS/DFS_1.java
+++ Inflean_Algorithm_2/DFS/DFS_1.java
@@ -5,19 +5,53 @@ import java.util.HashSet;
 
 // 1. 가장 가까운 큰수
 public class DFS_1 {
-	
+	static boolean flag;
+	static int answer,target,m;
+	static int nums[], ch [];
 	public static int solution(int n){
-		int answer = 0;
+		String str = String.valueOf(n);
+		flag = false;
+		answer = 0;
+		target = n;
+		m = str.length();
+		nums = new int [m];
+		// 체크 배열
+		ch = new int [m];
 		
+		for(int i = 0; i < m; i++) nums[i] = str.charAt(i) - '0';
+		Arrays.sort(nums);
+		DFS(0,0);
+		if(!flag) return -1;
+
 		return answer;
 	}
+
+	private static void DFS(int L, int number) {
 		
+		if(flag) return;
+		
+		if(L == m) {
+			if(number > target ) {
+				answer = number;
+				flag = true;
+			}
+			
+		}else {
+			for(int i = 0; i < m; i++) {
+				if(ch[i] == 0) {
+					
+					ch[i] = 1;
+					DFS(L+1,number * 10 +nums[i]);
+					ch[i] = 0;
+				}
+			}
+		}
+
+	}
+
 	public static void main(String[] args){
 		
 		System.out.println(solution(123));
-		System.out.println(solution(321));
-		System.out.println(solution(20573));
-		System.out.println(solution(27711));
-		System.out.println(solution(54312));
+		
 	}
 }
