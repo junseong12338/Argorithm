3fae461291968189121548bd1192bfb63efc7c8e
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-11-14 05:06:10 +0900

    11.14

--- Inflean_Algorithm_2/DFS/DFS_3.java
+++ Inflean_Algorithm_2/DFS/DFS_3.java
@@ -1,18 +1,44 @@
 package DFS;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 // 3. 바둑대회
-public class DFS_3 {
-	
+class DFS_3 {
+	static int N, answer;
+	static int[] ch;
+	public static void DFS(int L, int S, int[][] cans){
+		if(L == N/2){
+			ArrayList<Integer> A = new ArrayList<>();
+			ArrayList<Integer> B = new ArrayList<>();
+			for(int i = 0; i < N; i++){
+				if(ch[i] == 1) A.add(i);
+				else B.add(i);
+			}
+			int Asum = 0, Bsum = 0;
+			for(int i = 0; i < L; i++){
+				Asum += cans[A.get(i)][0];
+				Bsum += cans[B.get(i)][1];
+			}
+			answer = Math.min(answer, Math.abs(Asum - Bsum));
+		}
+		else{
+			for(int i = S; i < N; i++){
+				ch[i] = 1;
+				DFS(L + 1, i + 1, cans);
+				ch[i] = 0;
+			}
+		}
+	}
 	public static int solution(int[][] cans){
-		int answer = 0;
-		
+		answer = 1000000;
+		N = cans.length;
+		ch = new int[N];
+		DFS(0, 0, cans);
 		return answer;
 	}
 		
 	public static void main(String[] args){
+		
 		System.out.println(solution(new int[][]{{87, 84}, {66, 78}, {94, 94}, {93, 87}, {72, 92}, {78, 63}}));
 		System.out.println(solution(new int[][]{{10, 20}, {15, 25}, {35, 23}, {55, 20}}));
 		System.out.println(solution(new int[][]{{11, 27}, {16, 21}, {35, 21}, {52, 21}, {25, 33},{25, 32}, {37, 59}, {33, 47}}));
--- Inflean_Algorithm_2/DFS/DFS_4.java
+++ Inflean_Algorithm_2/DFS/DFS_4.java
@@ -1,16 +1,73 @@
 package DFS;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 
 // 4.팰린드롬의 경우의 수
 public class DFS_4 {
-	
+    // 문자를 맨앞 맨뒤에 넣다 뺐다 할때 편하게 사용하는 자료 구조
+	static Deque<Character> tmp;
+	static ArrayList<String> res;
+	static HashMap<Character, Integer> sH;
+	static int len;
 	public static String[] solution(String s){
-		String[] answer = {};
-
+		
+		sH = new HashMap<>();
+		tmp = new LinkedList<>();
+		res = new ArrayList<>();
+		len = s.length();
+		for(char x : s.toCharArray()) sH.put(x,sH.getOrDefault(x,0 )+1);
+		// 펠린드롬 생성 유무 확인 
+		int odd = 0;
+		char mid = '#';
+		for(char key : sH.keySet()) {
+			if(sH.get(key) %2 == 1) {
+				mid = key;
+				odd++;
+			}
+			
+		}
+		
+		if(odd > 1) return new String[] {};
+		if (mid != '#') {
+			tmp.add(mid);
+			sH.put(mid, sH.get(mid) - 1);
+		}
+		DFS();
+		String answer[] = new String[res.size()];
+		for(int  i = 0; i < res.size(); i++) answer[i] =  res.get(i);
+		
+		
 		return answer;
 	}
+
+	private static void DFS() {
+			if(tmp.size() == len) {
+				String Ts ="";
+				for(char x : tmp) Ts += x;
+				res.add(Ts);
+			}
+			else {
+				for(char key : sH.keySet()) {
+					if(sH.get(key) == 0 ) continue;
+					tmp.addFirst(key);
+					tmp.addLast(key);
+					sH.put(key, sH.get(key) - 2);
+					DFS();
+					tmp.pollFirst();
+					tmp.pollLast();
+					sH.put(key, sH.get(key) + 2);
+				}
+			}
+		}
+		
+
+
+
 
 	public static void main(String[] args){
 		System.out.println(Arrays.toString(solution("aaaabb")));	
