9829e83f0b198edce22038fea778e2e8743589ea
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2023-11-16 17:26:18 +0900

    11.16

--- Inflean_Algorithm_2/BFS/BFS_6.java
+++ Inflean_Algorithm_2/BFS/BFS_6.java
@@ -7,7 +7,48 @@ import java.util.*;
 public class BFS_6 {
 
 	public static int solution(int[][] board){
-		int answer = 0;
+		int answer = Integer.MAX_VALUE;
+		int dx[] = {-1,0,1,0};
+		int dy[] = {0,1,0,-1};
+		int n = board.length;
+		int m = board[0].length;
+		int dist[][] = new int [n][m];
+		
+		Queue<int[]> Q = new LinkedList<int[]>();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(board[i][j] == 2 || board[i][j] == 3) {
+					Q.offer(new int[] {i,j});
+					int [][] ch = new int [n][m];
+					ch[i][j] = 1;
+					int L = 0;
+					while(!Q.isEmpty()) {
+						L++;
+						int len = Q.size();
+						for(int r = 0; r < len; r++) {
+							int cur[] = Q.poll();
+							for(int k = 0; k < 4; k++) {
+								int nx = cur[0] + dx[k];
+								int ny = cur[1] + dy[k];
+								if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] != 1){
+									if(ch[nx][ny] == 0) {
+										ch[nx][ny] = 1;
+										dist[nx][ny] +=L;
+										Q.offer(new int[] {nx,ny});
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(board[i][j] == 4 && dist[i][j] > 0)answer = Math.min(answer, dist[i][j]);
+			}
+		}
 		
 		return answer;
 	}
